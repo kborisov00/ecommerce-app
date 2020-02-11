c8ae172ffd30ed3b53819f5b7e6e40a1134fa718
kborisov00 <krisokarts@abv.bg>
2020-02-11 18:24:12 +0200

    dots fixed/categories menu functional

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
+// Slider init
 $('.slider').slick({
     arrows: false,
     fade: true,
     autoplay: true,
     autoplaySpeed: 4000,
     dots: true,
+});
+
+// Header's categories menu
+$('.bot__categories').on('click', function () {
+    $('.bot__dropdown-menu').toggleClass('active');
 });
