b35c2761ae48e1dbcec8881e30ca7c6a2c112fb0
kborisov00 <krisokarts@abv.bg>
2020-02-11 19:47:39 +0200

    preloader added

--- js/main.js
+++ js/main.js
@@ -1,13 +1,18 @@
-// Slider init
-$('.slider').slick({
-    arrows: false,
-    fade: true,
-    autoplay: true,
-    autoplaySpeed: 4000,
-    dots: true,
-});
+$(document).ready(function () {
+    // Slider init
+    $('.slider').slick({
+        arrows: false,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        dots: true,
+    });
 
-// Header's categories menu
-$('.bot__categories').on('click', function () {
-    $('.bot__dropdown-menu').toggleClass('active');
+    // Header's categories menu
+    $('.bot__categories').on('click', function () {
+        $('.bot__dropdown-menu').toggleClass('active');
+    });
+
+    // hide preloader
+    $('.preloader').css('opacity', 0).css('visibility', 'hidden');
 });
